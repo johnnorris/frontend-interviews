a3666a5011d5caefb18b3b730c12527505132f41
norrisj <johntnorris@outlook.com>
2015-10-02 09:49:41 +0100

    Initial commit

---
+++ lunar-rover/js/script.js
@@ -0,0 +1,7 @@
+// Model the Moon, Lander and Rover as separate objects
+
+// The Lander can land on the Moon
+// The Lander will land at a specific position
+// The Rover can disembark the Lander
+// The Rover will disembark initially at the Landers position
+// The Rover can move around the Moon
