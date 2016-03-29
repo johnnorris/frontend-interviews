7335169fc327d9cb9bc864ef72272ef9b23b51c4
norrisj <johntnorris@outlook.com>
2016-03-29 13:17:13 +0100

    Removed spec

--- lunar-rover/test/script.spec.js
+++ lunar-rover/test/script.spec.js
@@ -2,54 +2,30 @@ var moon, lander, rover;
 
 beforeEach(function () {
     setFixtures('<div id="moon"></div><div id="lander"><div id="rover"></div></div>');
-
-    moon = new Moon($('#moon'));
-    lander = new Lander($('#lander'));
-    rover = new Rover($('#rover'), lander);
 });
 
 describe("The Lander", function () {
 
     it("can land on the Moon", function () {
-        lander.land(moon);
-
-        expect(moon.$element).toContainElement(lander.$element);
+        expect(false).toEqual(true);
     });
 
     it("will land at a specific position", function () {
-        var position = { top: 10, left: 20 };
-
-        lander.land(moon, position);
-
-        expect(lander.$element.position()).toEqual(position);
+        expect(false).toEqual(true);
     });
 });
 
 describe("The Rover", function () {
 
     it("can disembark the Lander", function () {
-        lander.land(moon);
-        rover.disembark();
-
-        expect(moon.$element).toContainElement(rover.$element);
+        expect(false).toEqual(true);
     });
 
     it("will disembark initially at the Landers position", function () {
-        var position = { top: 10, left: 20 };
-
-        lander.land(moon, position);
-        rover.disembark();
-
-        expect(rover.$element.position()).toEqual(position);
+        expect(false).toEqual(true);
     });
 
     it("can move around the Moon", function () {
-        var position = { top: 50, left: 43 };
-
-        lander.land(moon);
-        rover.disembark();
-        rover.move(position);
-
-        expect(rover.$element.position()).toEqual(position);
+        expect(false).toEqual(true);
     });
 });
