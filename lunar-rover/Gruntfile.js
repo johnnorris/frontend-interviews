649ca61b4ee36b1c6144c0ebd9896b52ae5f5b81
norrisj <johntnorris@outlook.com>
2016-06-15 12:15:15 +0100

    Updated to ES6 support

---
+++ lunar-rover/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        babel: {
+            options: {
+                sourceMap: false,
+                presets: ['es2015']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'es6',
+                    src: ['*.js'],
+                    dest: 'js'
+                }]
+            }
+        },
+        watch: {
+            js: {
+                files: ['es6/*.js'],
+                tasks: ['babel'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['babel', 'watch']);
+
+};
